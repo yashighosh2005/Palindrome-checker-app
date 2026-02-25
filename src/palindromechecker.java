802f977faca05abb5e2fa69433051813b9b5c513
yashighosh2005 <yashighosh2005@gmail.com>
2026-02-25 14:24:58 +0530

    feature 1

---
+++ src/palindromechecker.java
@@ -0,0 +1,9 @@
+public class palindromechecker {
+    public static void main(String[] args) {
+
+        // UC1
+        System.out.println("   Welcome to the Palindrome Checker App");
+        System.out.println("   Version : 1.0");
+        System.out.println("   System initialized successfully.");
+    }
+}
